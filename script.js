468cebe1d070508797731c7784be3e861731dfc9
GiulianaMagri <giulianamagri91@gmail.com>
2023-08-28 10:42:39 +0200

    Edit

--- script.js
+++ script.js
@@ -2,36 +2,48 @@ const countElement = document.getElementById("count");
 let count = 0;
 
 const counterContainer = document.querySelector(".counter-container");
-const buttonContainer = document.querySelector(".button-container")
+const buttonContainer = document.querySelector(".button-container");
 
-const incrementButton = document.createElement("button");
-incrementButton.textContent = ("+");
-incrementButton.classList.add("counter-button");
-incrementButton.addEventListener("click", () => {
-        count++;
+function createDOMElement(tagName, className, innerHTML) {
+    const element = document.createElement(tagName);
+    element.classList.add(className);
+    element.innerHTML = innerHTML;
+    return element;
+}
+
+const incrementButton = createDOMElement("button", "counter-button", "+");
+const decrementButton = createDOMElement("button", "counter-button", "-");
+const resetButton = createDOMElement("button", "reset-button", "remove");
+
+
+function buttonClick(event) {
+    const target = event.target;
+    
+    if (target.classList.contains("counter-button")) {
+        if (target.textContent === "+") {
+            count++;
+        } else if (target.textContent === "-") {
+            if (count > 0) {
+                count--;
+            }
+        }
+        
         countElement.textContent = count;
-});
-
-
-const decrementButton = document.createElement("button");
-decrementButton.textContent = ("-");
-decrementButton.classList.add("counter-button");
-decrementButton.addEventListener("click", () => {
-    if(count > 0) {
-    count--;
-    countElement.textContent = count;
+    } else if (target.classList.contains("reset-button")) {
+        count = 0;
+        countElement.textContent = count;
     }
-});
+}
 
 
-const resetButton = document.createElement("button");
-resetButton.textContent = ("remove");
-resetButton.classList.add("reset-button");
-resetButton.addEventListener("click", () => {
-    count=0;
-    countElement.textContent = count;
-});
+buttonContainer.addEventListener("click", buttonClick);
 
 buttonContainer.appendChild(incrementButton);
 buttonContainer.appendChild(decrementButton);
 buttonContainer.appendChild(resetButton);
+
+
+
+
+
+
